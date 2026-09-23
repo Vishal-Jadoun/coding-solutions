e5d5627ccab0c141db4687fe62d90e8afbdc5a51
Vishal Jadoun <vishal2791.beai25@chitkara.edu.in>
2026-09-23 19:32:55 +0530

    ✅ EXREST - Rating 123 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/EXREST---Rating-123/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int ans = a*3;
+		System.out.println(ans);
+	}
+}
