29b2dab1a95f39c9c4642964236226913c2bd5e6
Vishal Jadoun <vishal2791.beai25@chitkara.edu.in>
2026-09-23 19:29:12 +0530

    ✅ POPCORN7 - Rating 99 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/easy/POPCORN7---Rating-99/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int left = x-100;
+        int pop = left/50;
+        
+        System.out.println(pop);
+        
+       
+        
+        
+	}
+}
